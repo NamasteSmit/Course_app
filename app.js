4fb27311d2a61a7b91f0c156fe8c5eb01840dedd
NamasteSmit <smitpatel1305@gmial.com>
2024-08-27 22:21:56 -0700

    modified

--- app.js
+++ app.js
@@ -3,12 +3,14 @@
  const port = 3000;
  const bodyParser = require('body-parser');
 const adminRoutes = require('./routes/admin');
+const userRoutes = require('./routes/user');
 
 //middleware
 app.use(express.json());
 app.use(bodyParser.json());
 
 app.use('/admin', adminRoutes);
+app.use('/user',userRoutes);
 
 
  app.listen(port,()=>{
